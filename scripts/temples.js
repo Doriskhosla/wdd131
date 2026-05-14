07452805e09854f0c52318805ee0f0265737120c
Doriskhosla <doriskhosla@gmail.com>
2026-05-13 22:59:02 -0600

    update all the new changes in temple assignment

---
+++ scripts/temples.js
@@ -0,0 +1,12 @@
+document.querySelector("#year").textContent = new Date().getFullYear();
+document.querySelector("#lastModified").textContent = document.lastModified;
+
+const menuButton = document.querySelector("#menu");
+const navMenu = document.querySelector("#navMenu");
+
+menuButton.textContent = "☰";
+
+menuButton.addEventListener("click", () => {
+    navMenu.style.display = navMenu.style.display === "flex" ? "none" : "flex";
+    menuButton.textContent = menuButton.textContent === "☰" ? "✖" : "☰";
+});
