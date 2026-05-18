820159bee757790391dd862abbf33acf0de5e7a0
Doriskhosla <doriskhosla@gmail.com>
2026-05-18 17:46:40 -0600

    Update the new changes in week01

--- Week01/scripts/getdates.js
+++ Week01/scripts/getdates.js
@@ -1,5 +1,5 @@
-// Dynamic Year
+
 document.getElementById("CurrentYear").textContent = new Date().getFullYear();
 
-// Last Modified Date
+
 document.getElementById("LastModified").textContent = `Last Modified: ${document.lastModified}`;
